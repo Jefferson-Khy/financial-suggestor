8d90035a0282db9809a6997bc336aa2e41fccb34
jefferson-khy <khyjefferson@gmail.com>
2024-06-07 15:24:46 -0500

    aded space

--- easy-finance-client/src/App.js
+++ easy-finance-client/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+
 function App() {
   return (
     <div className="App">
